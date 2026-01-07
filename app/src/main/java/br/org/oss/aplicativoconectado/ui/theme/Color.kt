7046b9b21f7d1293bacf6b919de3fe51de12661b
Fabiano Oss <fabiano.oss@gmail.com>
2026-01-07 08:56:37 -0300

    Adjustments

--- app/src/main/java/br/org/oss/aplicativoconectado/ui/theme/Color.kt
+++ app/src/main/java/br/org/oss/aplicativoconectado/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package br.org.oss.aplicativoconectado.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Orange80 = Color(0xFFFFDCC1)
+val Brown80 = Color(0xFFD7C4B8)
+val Red80 = Color(0xFFF0C0C0)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Orange40 = Color(0xFFFF8A65)
+val Brown40 = Color(0xFF8D6E63)
+val Red40 = Color(0xFFE57373)
--- app/src/main/java/br/org/oss/aplicativoconectado/ui/theme/Theme.kt
+++ app/src/main/java/br/org/oss/aplicativoconectado/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = Brown80,
+    tertiary = Red80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange40,
+    secondary = Brown40,
+    tertiary = Red40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
